9ed17a5f24a8dcce4b892089a390a4bb40c4bcbf
Harshith Bezawada <>
2025-06-30 11:24:54 +0530

    1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose'
-import colors from 'colors'
-
+import colors from 'colors';
 // mongoose.connect returns a promise
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true
+      //useCreateIndex: true
     })
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
